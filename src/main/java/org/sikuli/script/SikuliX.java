7aa5a5f44ef18efd84a608384fbe7d06b3a1c7d6
Raimund Hocke <rmhdevelop@me.com>
2013-07-16 11:17:45 +0200

    minor revisions

--- src/main/java/org/sikuli/script/SikuliX.java
+++ src/main/java/org/sikuli/script/SikuliX.java
@@ -1,6 +1,8 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2010-2013, Sikuli.org
+ * Released under the MIT License.
+ *
+ * added RaiMan 2013
  */
 package org.sikuli.script;
 
@@ -14,6 +16,10 @@ import org.sikuli.basics.SikuliScript;
 public class SikuliX {
   private static final String me = "SikuliXFinalCleanUp: ";
   
+  /**
+   * can be used in IDE's to run scripts
+   * @param args
+   */
   public static void main(String[] args) {
     SikuliScript.main(args);
   }
@@ -21,19 +27,19 @@ public class SikuliX {
   public static void endNormal(int n) {
     Debug.log(3, me + "endNormal: %d", n);
     cleanup(0);
-    System.exit(1);
+    System.exit(n);
   }
   
   public static void endWarning(int n) {
-    Debug.log(3, me + "endNormal: %d", n);
+    Debug.log(3, me + "endWarning: %d", n);
     cleanup(0);
-    System.exit(1);
+    System.exit(n);
   }
   
   public static void endError(int n) {
-    Debug.log(3, me + "endNormal: %d", n);
+    Debug.log(3, me + "endError: %d", n);
     cleanup(0);
-    System.exit(1);
+    System.exit(n);
   }
     
   public static void endFatal(int n) {
@@ -43,10 +49,11 @@ public class SikuliX {
             "If you do not have any idea about the error cause or solution, run again\n" +
             "with a Debug level of 3. You might paste the output to the Q&A board.", n);
     cleanup(0);
-    System.exit(1);
+    System.exit(n);
   }
 
   private static void cleanup(int n) {
+    Debug.log(3, me + "cleanup: %d", n);
     ScreenHighlighter.closeAll();
     //TODO stop all background observers
   } 
