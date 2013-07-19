4c745e5c028a8d95f87cef336e5a3e384572a610
Raimund Hocke <rmhdevelop@me.com>
2013-07-19 18:23:14 +0200

    added a proc to be used after setup to test basic functions

--- src/main/java/org/sikuli/script/SikuliX.java
+++ src/main/java/org/sikuli/script/SikuliX.java
@@ -15,40 +15,42 @@ import org.sikuli.basics.SikuliScript;
  * @author rhocke
  */
 public class SikuliX {
+
   private static final String me = "SikuliXFinal: ";
-  
+
   /**
    * can be used in IDE's to run scripts
+   *
    * @param args
    */
   public static void main(String[] args) {
     SikuliScript.main(args);
   }
-  
+
   public static void endNormal(int n) {
     Debug.log(3, me + "endNormal: %d", n);
     cleanUp(0);
     System.exit(n);
   }
-  
+
   public static void endWarning(int n) {
     Debug.log(3, me + "endWarning: %d", n);
     cleanUp(0);
     System.exit(n);
   }
-  
+
   public static void endError(int n) {
     Debug.log(3, me + "endError: %d", n);
     cleanUp(0);
     System.exit(n);
   }
-    
+
   public static void endFatal(int n) {
-    Debug.error("Terminating SikuliX after a fatal error" +
-            (n == 0 ? "" : "(%d)" ) +
-            "! Sorry, but it makes no sense to continue!\n" +
-            "If you do not have any idea about the error cause or solution, run again\n" +
-            "with a Debug level of 3. You might paste the output to the Q&A board.", n);
+    Debug.error("Terminating SikuliX after a fatal error"
+            + (n == 0 ? "" : "(%d)")
+            + "! Sorry, but it makes no sense to continue!\n"
+            + "If you do not have any idea about the error cause or solution, run again\n"
+            + "with a Debug level of 3. You might paste the output to the Q&A board.", n);
     cleanUp(0);
     System.exit(n);
   }
@@ -60,5 +62,16 @@ public class SikuliX {
     if (CommandArgs.isIDE()) {
       //TODO reset selected options to defaults
     }
-  } 
+  }
+
+  public static boolean testSetup() {
+    Region r = Region.create(0, 0, 100, 100);
+    Pattern p = new Pattern(r.getScreen().capture(r));
+    Finder f = new Finder(p.getImage());
+    if (null != f.find(p)) {
+      SikuliScript.popup("Hallo from testSetup\nSikuli seems to be working fine!\n\nHave fun!");
+      return true;
+    }
+    return false;
+  }
 }
