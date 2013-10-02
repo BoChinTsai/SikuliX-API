a7e2c3e7ffdaeb9af33bd9dc6b075b090ac16dd9
Raimund Hocke <rmhdevelop@me.com>
2013-10-02 12:06:47 +0200

    added functions and tables to allow the evaluation of a non-US keyboard layout and create a mapping, that can be used with type() and write()

--- src/main/java/org/sikuli/script/Key.java
+++ src/main/java/org/sikuli/script/Key.java
@@ -6,18 +6,222 @@
  */
 package org.sikuli.script;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
 import org.sikuli.basics.Debug;
 import org.sikuli.basics.Image;
+import org.sikuli.basics.ImagePath;
+import org.sikuli.basics.KeyBoardSetupWindow;
 import org.sikuli.basics.Settings;
 
 public class Key {
 
-  //<editor-fold defaultstate="collapsed" desc="KeyNames to UniCode">
+  static String[] keyVK = new String[] {
+  //<editor-fold defaultstate="collapsed" desc="VK_xxx constant names and values Java 7">
+    "VK_0","48",
+    "VK_1","49",
+    "VK_2","50",
+    "VK_3","51",
+    "VK_4","52",
+    "VK_5","53",
+    "VK_6","54",
+    "VK_7","55",
+    "VK_8","56",
+    "VK_9","57",
+    "VK_A","65",
+    "VK_ACCEPT","30",
+    "VK_ADD","107",
+    "VK_AGAIN","65481",
+    "VK_ALL_CANDIDATES","256",
+    "VK_ALPHANUMERIC","240",
+    "VK_ALT","18",
+    "VK_ALT_GRAPH","65406",
+    "VK_AMPERSAND","150",
+    "VK_ASTERISK","151",
+    "VK_AT","512",
+    "VK_B","66",
+    "VK_BACK_QUOTE","192",
+    "VK_BACK_SLASH","92",
+    "VK_BACK_SPACE","8",
+    "VK_BEGIN","65368",
+    "VK_BRACELEFT","161",
+    "VK_BRACERIGHT","162",
+    "VK_C","67",
+    "VK_CANCEL","3",
+    "VK_CAPS_LOCK","20",
+    "VK_CIRCUMFLEX","514",
+    "VK_CLEAR","12",
+    "VK_CLOSE_BRACKET","93",
+    "VK_CODE_INPUT","258",
+    "VK_COLON","513",
+    "VK_COMMA","44",
+    "VK_COMPOSE","65312",
+    "VK_CONTEXT_MENU","525",
+    "VK_CONTROL","17",
+    "VK_CONVERT","28",
+    "VK_COPY","65485",
+    "VK_CUT","65489",
+    "VK_D","68",
+    "VK_DEAD_ABOVEDOT","134",
+    "VK_DEAD_ABOVERING","136",
+    "VK_DEAD_ACUTE","129",
+    "VK_DEAD_BREVE","133",
+    "VK_DEAD_CARON","138",
+    "VK_DEAD_CEDILLA","139",
+    "VK_DEAD_CIRCUMFLEX","130",
+    "VK_DEAD_DIAERESIS","135",
+    "VK_DEAD_DOUBLEACUTE","137",
+    "VK_DEAD_GRAVE","128",
+    "VK_DEAD_IOTA","141",
+    "VK_DEAD_MACRON","132",
+    "VK_DEAD_OGONEK","140",
+    "VK_DEAD_SEMIVOICED_SOUND","143",
+    "VK_DEAD_TILDE","131",
+    "VK_DEAD_VOICED_SOUND","142",
+    "VK_DECIMAL","110",
+    "VK_DELETE","127",
+    "VK_DIVIDE","111",
+    "VK_DOLLAR","515",
+    "VK_DOWN","40",
+    "VK_E","69",
+    "VK_END","35",
+    "VK_ENTER","10",
+    "VK_EQUALS","61",
+    "VK_ESCAPE","27",
+    "VK_EURO_SIGN","516",
+    "VK_EXCLAMATION_MARK","517",
+    "VK_F","70",
+    "VK_F1","112",
+    "VK_F10","121",
+    "VK_F11","122",
+    "VK_F12","123",
+    "VK_F13","61440",
+    "VK_F14","61441",
+    "VK_F15","61442",
+    "VK_F16","61443",
+    "VK_F17","61444",
+    "VK_F18","61445",
+    "VK_F19","61446",
+    "VK_F2","113",
+    "VK_F20","61447",
+    "VK_F21","61448",
+    "VK_F22","61449",
+    "VK_F23","61450",
+    "VK_F24","61451",
+    "VK_F3","114",
+    "VK_F4","115",
+    "VK_F5","116",
+    "VK_F6","117",
+    "VK_F7","118",
+    "VK_F8","119",
+    "VK_F9","120",
+    "VK_FINAL","24",
+    "VK_FIND","65488",
+    "VK_FULL_WIDTH","243",
+    "VK_G","71",
+    "VK_GREATER","160",
+    "VK_H","72",
+    "VK_HALF_WIDTH","244",
+    "VK_HELP","156",
+    "VK_HIRAGANA","242",
+    "VK_HOME","36",
+    "VK_I","73",
+    "VK_INPUT_METHOD_ON_OFF","263",
+    "VK_INSERT","155",
+    "VK_INVERTED_EXCLAMATION_MARK","518",
+    "VK_J","74",
+    "VK_JAPANESE_HIRAGANA","260",
+    "VK_JAPANESE_KATAKANA","259",
+    "VK_JAPANESE_ROMAN","261",
+    "VK_K","75",
+    "VK_KANA","21",
+    "VK_KANA_LOCK","262",
+    "VK_KANJI","25",
+    "VK_KATAKANA","241",
+    "VK_KP_DOWN","225",
+    "VK_KP_LEFT","226",
+    "VK_KP_RIGHT","227",
+    "VK_KP_UP","224",
+    "VK_L","76",
+    "VK_LEFT","37",
+    "VK_LEFT_PARENTHESIS","519",
+    "VK_LESS","153",
+    "VK_M","77",
+    "VK_META","157",
+    "VK_MINUS","45",
+    "VK_MODECHANGE","31",
+    "VK_MULTIPLY","106",
+    "VK_N","78",
+    "VK_NONCONVERT","29",
+    "VK_NUM_LOCK","144",
+    "VK_NUMBER_SIGN","520",
+    "VK_NUMPAD0","96",
+    "VK_NUMPAD1","97",
+    "VK_NUMPAD2","98",
+    "VK_NUMPAD3","99",
+    "VK_NUMPAD4","100",
+    "VK_NUMPAD5","101",
+    "VK_NUMPAD6","102",
+    "VK_NUMPAD7","103",
+    "VK_NUMPAD8","104",
+    "VK_NUMPAD9","105",
+    "VK_O","79",
+    "VK_OPEN_BRACKET","91",
+    "VK_P","80",
+    "VK_PAGE_DOWN","34",
+    "VK_PAGE_UP","33",
+    "VK_PASTE","65487",
+    "VK_PAUSE","19",
+    "VK_PERIOD","46",
+    "VK_PLUS","521",
+    "VK_PREVIOUS_CANDIDATE","257",
+    "VK_PRINTSCREEN","154",
+    "VK_PROPS","65482",
+    "VK_Q","81",
+    "VK_QUOTE","222",
+    "VK_QUOTEDBL","152",
+    "VK_R","82",
+    "VK_RIGHT","39",
+    "VK_RIGHT_PARENTHESIS","522",
+    "VK_ROMAN_CHARACTERS","245",
+    "VK_S","83",
+    "VK_SCROLL_LOCK","145",
+    "VK_SEMICOLON","59",
+    "VK_SEPARATER","108",
+    "VK_SEPARATOR","108",
+    "VK_SHIFT","16",
+    "VK_SLASH","47",
+    "VK_SPACE","32",
+    "VK_STOP","65480",
+    "VK_SUBTRACT","109",
+    "VK_T","84",
+    "VK_TAB","9",
+    "VK_U","85",
+    "VK_UNDEFINED","0",
+    "VK_UNDERSCORE","523",
+    "VK_UNDO","65483",
+    "VK_UP","38",
+    "VK_V","86",
+    "VK_W","87",
+    "VK_WINDOWS","524",
+    "VK_X","88",
+    "VK_Y","89",
+    "VK_Z","90"
+  //</editor-fold>
+  };
+  
+  // non function keys on US-Keyboard per line top down without modifier keys
+  public static String keyboardUS = 
+          "1234567890-=" + "qwertyuiop[]" + "asdfghjkl;'\\" + "`zxcvbnm,./";
+  
+  //<editor-fold defaultstate="collapsed" desc="KeyNames to UniCode (used in type() with Key.XXX)">
   public static final String SPACE = " ";
   public static final String ENTER = "\n";
   public static final String BACKSPACE = "\b";
@@ -141,8 +345,10 @@ public class Key {
 //</editor-fold>
 
   private static Map<String, Integer> keyTexts = new HashMap<String, Integer>();
+  private static Map<Integer, String> keys = new HashMap<Integer, String>();
 
   static {
+    //<editor-fold defaultstate="collapsed" desc="create the keyname map used with write()">
     String sKey;
     keyMaxLength = 0;
     for (char c = cMin; c < cMax; c++) {
@@ -153,34 +359,44 @@ public class Key {
       }
     }
     keyTexts.put("#ENTER.", toJavaKeyCode('\n')[0]);
-    keyTexts.put("#N.", toJavaKeyCode('\n')[0]);    
+    keyTexts.put("#N.", toJavaKeyCode('\n')[0]);
     keyTexts.put("#BACK.", toJavaKeyCode('\b')[0]);
     keyTexts.put("#B.", toJavaKeyCode('\b')[0]);
     keyTexts.put("#TAB.", toJavaKeyCode('\t')[0]);
     keyTexts.put("#T.", toJavaKeyCode('\t')[0]);
     keyTexts.put("#X.", toJavaKeyCode(C_NEXT)[0]);
-    keyTexts.put("#ESC.", toJavaKeyCode(C_ESC)[0]);    
-    keyTexts.put("#U.", toJavaKeyCode(C_UP)[0]);    
-    keyTexts.put("#D.", toJavaKeyCode(C_DOWN)[0]);    
-    keyTexts.put("#L.", toJavaKeyCode(C_LEFT)[0]);    
-    keyTexts.put("#R.", toJavaKeyCode(C_RIGHT)[0]);    
-    keyTexts.put("#S.", toJavaKeyCode(C_SHIFT)[0]);    
-    keyTexts.put("#S+", toJavaKeyCode(C_SHIFT)[0]);    
-    keyTexts.put("#S-", toJavaKeyCode(C_SHIFT)[0]);    
-    keyTexts.put("#A.", toJavaKeyCode(C_ALT)[0]);    
-    keyTexts.put("#A+", toJavaKeyCode(C_ALT)[0]);    
-    keyTexts.put("#A-", toJavaKeyCode(C_ALT)[0]);    
-    keyTexts.put("#C.", toJavaKeyCode(C_CTRL)[0]);    
-    keyTexts.put("#C+", toJavaKeyCode(C_CTRL)[0]);    
-    keyTexts.put("#C-", toJavaKeyCode(C_CTRL)[0]);    
-    keyTexts.put("#M.", toJavaKeyCode(C_META)[0]);    
-    keyTexts.put("#M+", toJavaKeyCode(C_META)[0]);    
-    keyTexts.put("#M-", toJavaKeyCode(C_META)[0]); 
-    if (Debug.getDebugLevel() > 3) {
-      for (String k : keyTexts.keySet()) {
+    keyTexts.put("#ESC.", toJavaKeyCode(C_ESC)[0]);
+    keyTexts.put("#U.", toJavaKeyCode(C_UP)[0]);
+    keyTexts.put("#D.", toJavaKeyCode(C_DOWN)[0]);
+    keyTexts.put("#L.", toJavaKeyCode(C_LEFT)[0]);
+    keyTexts.put("#R.", toJavaKeyCode(C_RIGHT)[0]);
+    keyTexts.put("#S.", toJavaKeyCode(C_SHIFT)[0]);
+    keyTexts.put("#S+", toJavaKeyCode(C_SHIFT)[0]);
+    keyTexts.put("#S-", toJavaKeyCode(C_SHIFT)[0]);
+    keyTexts.put("#A.", toJavaKeyCode(C_ALT)[0]);
+    keyTexts.put("#A+", toJavaKeyCode(C_ALT)[0]);
+    keyTexts.put("#A-", toJavaKeyCode(C_ALT)[0]);
+    keyTexts.put("#C.", toJavaKeyCode(C_CTRL)[0]);
+    keyTexts.put("#C+", toJavaKeyCode(C_CTRL)[0]);
+    keyTexts.put("#C-", toJavaKeyCode(C_CTRL)[0]);
+    keyTexts.put("#M.", toJavaKeyCode(C_META)[0]);
+    keyTexts.put("#M+", toJavaKeyCode(C_META)[0]);
+    keyTexts.put("#M-", toJavaKeyCode(C_META)[0]);
+    for (String k : keyTexts.keySet()) {
+      if (Debug.getDebugLevel() > 3) {
         Debug.log(4, "Key: %s is: %s", k, KeyEvent.getKeyText(keyTexts.get(k)));
       }
+      if (k.length() < 4) {
+        continue;
+      }
+      keys.put(keyTexts.get(k), k);
     }
+    //</editor-fold>
+  }
+  
+  //<editor-fold defaultstate="collapsed" desc="support functions for write()">
+  public static String getTextFromKeycode(int key) {
+    return keys.get(key);
   }
   
   public static boolean isRepeatable(String token) {
@@ -197,6 +413,25 @@ public class Key {
     }
     return false;
   }
+  
+  public static boolean isModifier(String token) {
+    if (toJavaKeyCodeFromText(token) == toJavaKeyCodeFromText("#S.") ||
+        toJavaKeyCodeFromText(token) == toJavaKeyCodeFromText("#C.") ||
+        toJavaKeyCodeFromText(token) == toJavaKeyCodeFromText("#A.") ||
+        toJavaKeyCodeFromText(token) == toJavaKeyCodeFromText("#M.")) {
+      return true;
+    }
+    return false;
+  }
+  
+  public static int toJavaKeyCodeFromText(String key) {
+    if (null == keyTexts.get(key)) {
+      return -1;
+    } else {
+      return keyTexts.get(key).intValue();
+    }
+  }
+  //</editor-fold>
   
   /**
    * Convert Sikuli Key to Java virtual key code
@@ -268,6 +503,7 @@ public class Key {
       case 'Y': return new int[]{KeyEvent.VK_SHIFT, KeyEvent.VK_Y};
       case 'Z': return new int[]{KeyEvent.VK_SHIFT, KeyEvent.VK_Z};
 //Row 3 (below function keys)
+//      case '§': return new int[]{192}; //not producable
       case '1': return new int[]{KeyEvent.VK_1};
       case '!': return new int[]{KeyEvent.VK_SHIFT, KeyEvent.VK_1};
       case '2': return new int[]{KeyEvent.VK_2};
@@ -377,13 +613,13 @@ public class Key {
 //Num pad special
       case Key.C_SEPARATOR: return new int[]{KeyEvent.VK_SEPARATOR};
       case Key.C_ADD:       return new int[]{KeyEvent.VK_ADD};
-      case Key.C_MINUS:     return new int[]{KeyEvent.VK_MINUS};
+      case Key.C_MINUS:     return new int[]{KeyEvent.VK_SUBTRACT};
       case Key.C_MULTIPLY:  return new int[]{KeyEvent.VK_MULTIPLY};
       case Key.C_DIVIDE:    return new int[]{KeyEvent.VK_DIVIDE};
       case Key.C_DECIMAL:   return new int[]{KeyEvent.VK_DECIMAL};
       case Key.C_CONTEXT:   return new int[]{KeyEvent.VK_CONTEXT_MENU};
       case Key.C_WIN:   return new int[]{KeyEvent.VK_WINDOWS};
-// tab in GUI
+//hack: alternative tab in GUI
       case Key.C_NEXT:   return new int[]{-KeyEvent.VK_TAB};
 
       default:
@@ -396,7 +632,7 @@ public class Key {
    * @param key
    * @return a printable version of a special key
    */
-  protected static String toJavaKeyCodeText(char key) {
+  public static String toJavaKeyCodeText(char key) {
     switch (key) {
 //RETURN, BACKSPACE, TAB
       case '\b': return "#BACK.";
@@ -469,11 +705,7 @@ public class Key {
         return "" + key;
     }
   }
-  
-  public static int toJavaKeyCodeFromText(String key) {
-    return keyTexts.get(key);
-  }
-  
+    
   protected static int convertModifiers(String mod) {
     int modNew = 0;
     char key;
@@ -525,4 +757,228 @@ public class Key {
       return KeyEvent.VK_CONTROL;
     }
   }
+  
+  //<editor-fold defaultstate="collapsed" desc="keyboard setup - INTERNAL USE ONLY">
+  static String[] result = new String[] {""};
+  
+  /**
+   * INTERNAL USE ONLY
+   * create an alternate keycode table for a non-US keyboard
+   */
+  public static void keyBoardSetup() throws FindFailed, Exception {
+    Map<Character, Integer[]> keyLocal = new HashMap<Character, Integer[]>();
+    String[] keyXX = new String[] {"", "", "", "",};
+    String keysx = Key.keyboardUS;
+
+    Screen s = new Screen(0);
+    ImagePath.add("org.sikuli.script.SikuliX/images");
+
+    ShowKeyBoardSetupWindow win = new ShowKeyBoardSetupWindow();
+    win.start();
+    s.wait(3.0F);
+
+    Location btnOK = s.find("SikuliLogo").getCenter();
+    Location txtArea = btnOK.offset(0, 200);
+    s.click(txtArea);
+    s.wait(1.0F);
+    String[] mods = new String[]{"", "S", "A", "SA"};
+    for (String modx : mods) {
+      s.paste((modx.isEmpty() ? "NO" : modx) + " modifier" + "\n");
+      if (!modx.isEmpty()) {
+        if (modx.length() == 1) {
+          modx = "#" + modx + ".";
+        } else if (modx.length() == 2) {
+          modx = "#" + modx.substring(0, 1) + ".#" + modx.substring(1, 2) + ".";
+        } else {
+          break;
+        }
+      }
+      String c;
+      for (int n = 0; n < keysx.length(); n++) {
+        c = "" + keysx.charAt(n);
+        s.paste(c);
+        s.type(" ");
+        s.write(String.format("%s%s ", modx, c));
+        if ("=".equals(c) || "]".equals(c) || "\\".equals(c)) {
+          s.paste("\n");
+        }
+      }
+      s.paste("\n");
+      s.type(Key.ENTER);
+    }
+    s.click(btnOK);
+    while (true) {
+      s.wait(1.0F);
+      if (!result[0].isEmpty()) {
+        break;
+      }
+    }
+    String[] keyNewx = result[0].split("\n");
+    String keysNew;
+    String mod;
+    int nKDE = 0;
+    for (int n = 0; n < keyNewx.length; n++) {
+      mod = keyNewx[n].substring(0, 2);
+      keysNew = keyNewx[n + 1] + keyNewx[n + 2] + keyNewx[n + 3] + keyNewx[n + 4];
+      keyXX[nKDE] = mod + ": " + keysNew;
+      //System.out.println(keyXX[nKDE]);
+      n += 4;
+      nKDE++;
+    }
+    String kSet;
+    int offset = 4;
+    char keyOld, keyNew;
+    int[] codeA;
+    String codeS;
+    String modText;
+    int nOld;
+    for (int n = 0; n < 4; n++) {
+      kSet = keyXX[n].substring(4);
+      mod = keyXX[n].substring(0, 2);
+      modText = "";
+      if ("S ".equals(mod)) {
+        modText = "SHIFT ";
+      } else if ("A ".equals(mod)) {
+        modText = "ALT ";
+      } else if ("SA".equals(mod)) {
+        modText = "SHIFT ALT ";
+      }
+      //System.out.println(mod + "\n" + kSet);
+      nOld = 0;
+      for (int i = 0; i < kSet.length(); i++) {
+        if (!" ".equals("" + kSet.charAt(i + 3))) {
+          offset = 3;
+        }
+        keyOld = kSet.charAt(i);
+        keyNew = kSet.charAt(i + 2);
+        Integer[] codeI = new Integer[]{-1, -1, -1};
+        try {
+          codeA = Key.toJavaKeyCode(keyNew);
+          codeS = "";
+          for (int iK : codeA) {
+            codeS += KeyEvent.getKeyText(iK) + " ";
+          }
+          if (codeA.length == 1) {
+            codeI[2] = codeA[0];
+          } else if (codeA.length == 2) {
+            codeI[2] = codeA[1];
+            codeI[0] = codeA[0];
+          } else if (codeA.length == 3) {
+            codeI[2] = codeA[2];
+            codeI[0] = codeA[0];
+            codeI[1] = codeA[1];
+          }
+        } catch (Exception e) {
+          codeS = "UNKNOWN ";
+          codeI[2] = Key.toJavaKeyCode(keysx.charAt(nOld))[0];
+          codeS += modText + KeyEvent.getKeyText(codeI[2]);
+          for (int ik = 0; ik < 2; ik++) {
+            if (mod.charAt(ik) == 'S') {
+              codeI[ik] = KeyEvent.VK_SHIFT;
+            } else if (mod.charAt(ik) == 'A') {
+              codeI[ik] = KeyEvent.VK_ALT;
+            }
+          }
+        }
+        if (keyLocal.get(keyNew) == null) {
+          keyLocal.put(keyNew, codeI);
+        }
+        //System.out.println(String.format("%s %c %c %s", mod, keyOld, keyNew, codeS));
+        i += offset - 1;
+        offset = 4;
+        nOld += 1;
+      }
+    }
+    
+    System.out.println("--------------- keyLocal");
+    Integer[] codeI;
+    for (Character kc : keyLocal.keySet()) {
+      codeI = keyLocal.get(kc);
+      System.out.println(String.format("%c %s %s %s", kc.charValue(),
+              (codeI[0] == -1 ? "" : KeyEvent.getKeyText(codeI[0])),
+              (codeI[1] == -1 ? "" : KeyEvent.getKeyText(codeI[1])),
+              (codeI[2] == -1 ? "" : KeyEvent.getKeyText(codeI[2])) ));
+    }
+  }
+  
+  static class ShowKeyBoardSetupWindow extends Thread {
+    
+    JFrame kbSetup;
+    
+    @Override
+    public void run() {
+      kbSetup = new JFrame("Localized Keyboard Setup");
+      Container mpwinCP = kbSetup.getContentPane();
+      mpwinCP.setLayout(new BorderLayout());
+      KeyBoardSetupWindow win = new KeyBoardSetupWindow(result);
+      mpwinCP.add(win, BorderLayout.CENTER);
+      kbSetup.pack();
+      kbSetup.setAlwaysOnTop(true);
+      kbSetup.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+      win.setLogo(new ImageIcon(Image.create("SikuliLogo").getImage()));
+      kbSetup.setVisible(true);
+    }
+    
+    public void setResult(String text) {
+      kbSetup.setVisible(false);
+      result[0] = text;
+    }
+  }
+  
+  /**
+   * INTERNAL USE ONLY
+   * create a table containing all relevant key, keycode and keytext settings for VK_xxx
+   */
+  public static void createKeyTable() {
+    Map<String, String> namesVK = new HashMap<String, String>();
+    for (int i = 0; i < keyVK.length; i += 2) {
+      namesVK.put(keyVK[i+1], keyVK[i].substring(3));
+    }
+    
+    String keyText;
+    int upper = 66000;
+    String form = "%05d: %-25s %-20s %-15s %15s %s";
+    for (int key = 0; key < upper; key++) {
+      char ckey = (char) key;
+      String k1 = "...";
+      String k2 = "...";
+      try {
+        if (toJavaKeyCode((ckey)).length > 1) {
+                  k1 = namesVK.get(new Integer(toJavaKeyCode(ckey)[0]).toString());
+                  k2 = namesVK.get(new Integer(toJavaKeyCode(ckey)[1]).toString());
+        } else if (key > 32) {
+          k2 = namesVK.get(new Integer(toJavaKeyCode(ckey)[0]).toString());
+        }
+      } catch (Exception e) {      }
+      keyText = KeyEvent.getKeyText(key);
+      if (keyText.startsWith("Unknown") && ("...".equals(k2) || key > 255)) {
+        continue;
+      }
+      if (keyText.startsWith("Unknown")) {
+        keyText = "???";
+      }
+      if (keyText.length() == 1) {
+        if (key < 256 ) {
+          if (key < 32) {
+            key = ' ';
+          }
+          System.out.println(String.format(form, key, keyText, "...", "...", k2, k1));
+        }
+        continue;
+      }
+      keyText = keyText.replaceAll(" ", "_");
+      String vkey = namesVK.get(new Integer(key).toString());
+      if (vkey == null || vkey.equals(keyText.toUpperCase())) {
+        vkey = "...";
+      }
+      if (null == getTextFromKeycode(key)) {
+        System.out.println(String.format(form, key, keyText.toUpperCase(), 
+                vkey, "...", k2, k1));
+      } else {
+        System.out.println(String.format(form, key, keyText.toUpperCase(), 
+                vkey, getTextFromKeycode(key), k2, k1));
+      }
+    }
+  }
+  //</editor-fold>
 }
