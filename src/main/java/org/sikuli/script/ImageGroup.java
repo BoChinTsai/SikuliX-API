6cddf6ca2121d4a15c44472a24e2e8f71558af51
Raimund Hocke <rmhdevelop@me.com>
2013-11-23 11:34:36 +0100

    added ImageGroup for features on a group of images stored in a folder

---
+++ src/main/java/org/sikuli/script/ImageGroup.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2010-2013, Sikuli.org
+ * Released under the MIT License.
+ *
+ * added RaiMan 2013
+ */
+package org.sikuli.script;
+
+/**
+ * An ImageGroup object represents images, that are all in the same folder.<br />
+ * The folder will be found on the image path.<br />
+ * The match data of the images can be stored and reloaded.<br />
+ * Its main purpose is to support image sets for different environments.<br />
+ * At runtime you can switch between image sets without changing the used image names.<br />
+ * Based on the stored match data you might produce a new ImageGroup on the fly.<br />
+ * 
+ * @author RaiMan
+ */
+public class ImageGroup {
+  
+}
